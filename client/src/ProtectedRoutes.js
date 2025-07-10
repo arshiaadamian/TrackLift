4d62f9db7bd96f94a405dff214ad1ef25de66348
Arshia Adamian <arshiaadamian@gmail.com>
2025-07-09 20:38:57 -0700

    fixed the api handling in the ProtectedRoutes.js file

--- client/src/ProtectedRoutes.js
+++ client/src/ProtectedRoutes.js
@@ -2,9 +2,11 @@ import { Outlet, Navigate } from "react-router-dom";
 import React, { useEffect, useState } from "react";
 
 const ProtectedRoutes = () => {
+  const API_URL = process.env.REACT_APP_API_URL || "";
   const [auth, setAuth] = useState(null);
+
   useEffect(() => {
-    fetch("/api/user")
+    fetch(`${API_URL}/api/user`)
       .then((res) => res.json())
       .then((data) => {
         if (data.username) {
