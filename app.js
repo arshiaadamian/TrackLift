a3b81d0629dc36b62b77e1573abff164f4f0745a
Arshia Adamian <arshiaadamian@gmail.com>
2025-07-11 11:50:29 -0700

    test

--- app.js
+++ app.js
@@ -110,8 +110,8 @@ app.get("/test", (req, res) => {
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client/build")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  app.get("/*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client/build", "index.html"));
   });
 }
 
