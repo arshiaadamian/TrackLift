dc2a8caa0710d70ae8b67048c2885772e084801f
Arshia Adamian <arshiaadamian@gmail.com>
2025-07-11 19:00:49 -0700

    changed the login page

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,56 +1,83 @@
-import React from "react";
+import React, { useState } from "react";
 import "../style/login.css";
-import { Link, useLocation } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import logo from "../images/logo.png";
 
 function Login() {
-  const location = useLocation();
-  const params = new URLSearchParams(location.search);
-  const error = params.get("error");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    const formData = new URLSearchParams();
+    formData.append("username", username);
+    formData.append("password", password);
+
+    try {
+      const res = await fetch(
+        "https://tracklift-server.onrender.com/loggingin",
+        {
+          method: "POST",
+          body: formData,
+          credentials: "include",
+        }
+      );
+
+      if (res.ok) {
+        navigate("/");
+      } else {
+        const data = await res.json();
+        setError(data.error);
+      }
+    } catch (err) {
+      setError("Something went wrong. Try again.");
+    }
+  };
 
   return (
     <div className="body">
-      <form className="form-signin" action="/loggingin" method="POST">
+      <form className="form-signin" onSubmit={handleLogin}>
         <img className="mb-4" src={logo} alt="logo" width="140" height="140" />
         <h1 className="h3 mb-3 font-weight-normal">Sign in</h1>
-        <label htmlFor="inputEmail" className="sr-only"></label>
+
         <input
           type="email"
-          id="inputEmail"
           className="form-control"
           placeholder="Email address"
-          required=""
-          autoFocus=""
-          name="username"
+          required
+          autoFocus
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
         />
-        <label htmlFor="inputPassword" className="sr-only"></label>
+
         <input
           type="password"
-          id="inputPassword"
           className="form-control"
           placeholder="Password"
-          required=""
-          name="password"
+          required
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
         />
 
         <button className="btn btn-lg btn-primary btn-block" type="submit">
           Sign in
         </button>
-        <div className="error-message">
-          {error === "user_not_exist" && (
-            <p style={{ color: "red" }} className="error">
-              user does not exist
-            </p>
-          )}
-          {error === "incorrect_password" && (
-            <p style={{ color: "red" }} className="error">
-              incorrect password
-            </p>
-          )}
+
+        <div className="error-message" style={{ color: "red" }}>
+          {error === "user_not_exist" && <p>User does not exist</p>}
+          {error === "incorrect_password" && <p>Incorrect password</p>}
+          {error &&
+            error !== "user_not_exist" &&
+            error !== "incorrect_password" && <p>{error}</p>}
         </div>
+
         <Link to="/signup" className="signup-link">
-          Don't have an account? click here to sign up
+          Don't have an account? Click here to sign up
         </Link>
+
         <p className="mt-5 mb-3 text-muted">
           All rights reserved to the author, Arshia Adamian.
         </p>
